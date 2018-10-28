cb583aca782bad0cc9474ded516de159d2402bb0
jonpawlowski <13789244+jonpawlowski@users.noreply.github.com>
2018-10-28 17:13:39 -0400

    Pull the right charges

--- app.js
+++ app.js
@@ -25,18 +25,44 @@ app.set('view engine', 'ejs');
 const chargeRouter = require('./src/routes/chargeRoutes')();
 const analysisRouter = require('./src/routes/analysisRoutes')();
 const authRouter = require('./src/routes/authRoutes')();
+const { MongoClient } = require('mongodb');
 
 app.use('/charges', chargeRouter);
 app.use('/analysis', analysisRouter);
 app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
-  res.render(
-    'index'
-  );
-})
+  const url = 'mongodb://localhost:27017';
+  const dbName = 'financeApp';
 
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+      debug('Connected correctly to server for index retrieval');
+
+      const db = client.db(dbName);
+      const col = await db.collection('charges');
+      const recentCharges = await col.find({
+        "chargeDate" : {
+          $lt: new Date(),
+          $gte: new Date(new Date().setDate(new Date().getDate()-7))
+        }
+      }).toArray();
+
+  res.render(
+    'index',
+    {
+      recentCharges
+    }
+  );
+  } catch(err) {
+    debug(err.stack);
+  }
+  client.close();
+  }());
+});
 
 app.listen(port, function(){
-  debug(`listening on port ${chalk.green('port')}`);
+  debug(`listening on port ${chalk.green(port)}`);
 });
--- src/routes/chargeRoutes.js
+++ src/routes/chargeRoutes.js
@@ -6,9 +6,30 @@ const chargeRouter = express.Router();
 function router() {
   chargeRouter.route('/')
     .get((req, res) => {
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'financeApp';
+
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected correctly to server for charge retrieval');
+
+          const db = client.db(dbName);
+          const col = await db.collection('charges');
+          const charges = await col.find().toArray();
+
       res.render(
-        'chargesView'
+        'chargesView',
+        {
+          charges
+        }
       );
+    } catch(err) {
+      debug(err.stack);
+    }
+    client.close();
+    }());
   });
 
   return chargeRouter;
