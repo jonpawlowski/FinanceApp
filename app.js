f166dc4a1fb11e04384d115f6af597d8a576294f
jonpawlowski <13789244+jonpawlowski@users.noreply.github.com>
2018-10-30 23:50:36 -0400

    added new styles

--- app.js
+++ app.js
@@ -56,10 +56,27 @@ app.get('/', (req, res) => {
         var dateB = new Date(b.chargeDate);
         return dateB - dateA;
       });
+      var d = new Date();
+      var month = new Array();
+      month[0] = "January";
+      month[1] = "February";
+      month[2] = "March";
+      month[3] = "April";
+      month[4] = "May";
+      month[5] = "June";
+      month[6] = "July";
+      month[7] = "August";
+      month[8] = "September";
+      month[9] = "October";
+      month[10] = "November";
+      month[11] = "December";
+      var currentMonth = month[d.getMonth()];
+
   res.render(
     'test',
     {
-      recentCharges
+      recentCharges,
+      currentMonth
     }
   );
   } catch(err) {
