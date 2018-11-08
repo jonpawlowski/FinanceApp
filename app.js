077f3aef562a7bb387c35f8df91e933f50d871a8
jonpawlowski <13789244+jonpawlowski@users.noreply.github.com>
2018-11-08 13:01:07 -0500

    Added default router and finished budget calculations

--- app.js
+++ app.js
@@ -22,69 +22,17 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+const defaultRouter = require('./src/routes/defaultRoutes')();
 const chargeRouter = require('./src/routes/chargeRoutes')();
 const analysisRouter = require('./src/routes/analysisRoutes')();
 const authRouter = require('./src/routes/authRoutes')();
 const newChargesRouter = require('./src/routes/newChargesRoutes')();
-const { MongoClient } = require('mongodb');
 
+app.use('/', defaultRouter);
 app.use('/charges', chargeRouter);
 app.use('/analysis', analysisRouter);
 app.use('/auth', authRouter);
 app.use('/newCharges', newChargesRouter);
-
-app.get('/', (req, res) => {
-  const url = 'mongodb://localhost:27017';
-  const dbName = 'financeApp';
-
-  (async function mongo() {
-    let client;
-    try {
-      client = await MongoClient.connect(url);
-      debug('Connected correctly to server for index retrieval');
-
-      const db = client.db(dbName);
-      const col = await db.collection('charges');
-      const recentCharges = await col.find({
-        "chargeDate" : {
-          $lt: new Date(),
-          $gte: new Date(new Date().setDate(new Date().getDate()-7))
-        }
-      }).toArray();
-      recentCharges.sort(function compare(a, b) {
-        var dateA = new Date(a.chargeDate);
-        var dateB = new Date(b.chargeDate);
-        return dateB - dateA;
-      });
-      var d = new Date();
-      var month = new Array();
-      month[0] = "January";
-      month[1] = "February";
-      month[2] = "March";
-      month[3] = "April";
-      month[4] = "May";
-      month[5] = "June";
-      month[6] = "July";
-      month[7] = "August";
-      month[8] = "September";
-      month[9] = "October";
-      month[10] = "November";
-      month[11] = "December";
-      var currentMonth = month[d.getMonth()];
-
-  res.render(
-    'test',
-    {
-      recentCharges,
-      currentMonth
-    }
-  );
-  } catch(err) {
-    debug(err.stack);
-  }
-  client.close();
-  }());
-});
 
 app.listen(port, function(){
   debug(`listening on port ${chalk.green(port)}`);
---
+++ src/routes/defaultRoutes.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:defaultRoutes');
+const defaultRouter = express.Router();
+
+function router() {
+  defaultRouter.route('/')
+  .get((req, res) => {
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'financeApp';
+
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug('Connected correctly to server for index retrieval');
+
+        const db = client.db(dbName);
+        const col = await db.collection('charges');
+        const recentCharges = await col.find({
+          "chargeDate" : {
+            $lt: new Date(),
+            $gte: new Date(new Date().setDate(new Date().getDate()-7))
+          }
+        }).toArray();
+        // Sort charges by newest first
+        recentCharges.sort(function compare(a, b) {
+          var dateA = new Date(a.chargeDate);
+          var dateB = new Date(b.chargeDate);
+          return dateB - dateA;
+        });
+        debug('Connected for recent charges successfully')
+        // Get current monthly dollars spent as of today
+        const date = new Date();
+        const monthlyCharges = await col.find({
+          "chargeDate" : {
+            $lt: new Date(),
+            $gte: new Date(date.getFullYear(), date.getMonth(), 1)
+          }
+        }).toArray();
+
+        // Perform current budget performance
+        var totalMonthlyCharges = 0;
+        var currentOnBudget = 0;
+        const monthlyBudget = 2000;
+        const lastOfMonth = new Date( date.getFullYear(), date.getMonth()+1, 0 );
+        const numDays = lastOfMonth.getDate();
+
+        for (i = 0; i < monthlyCharges.length; i++) {
+          totalMonthlyCharges += monthlyCharges[i].amount;
+        }
+        const budgetRemaining = monthlyBudget - totalMonthlyCharges;
+        currentOnBudget = ((monthlyBudget/numDays) * date.getDate()).toFixed(2);
+        //debug(`on Budget = ${currentOnBudget}`)
+        // Get current month to display on index page
+        const d = new Date();
+        const month = new Array();
+        month[0] = "January";
+        month[1] = "February";
+        month[2] = "March";
+        month[3] = "April";
+        month[4] = "May";
+        month[5] = "June";
+        month[6] = "July";
+        month[7] = "August";
+        month[8] = "September";
+        month[9] = "October";
+        month[10] = "November";
+        month[11] = "December";
+        const currentMonth = month[d.getMonth()];
+
+    res.render(
+      'test',
+      {
+        recentCharges,
+        currentMonth,
+        totalMonthlyCharges,
+        budgetRemaining,
+        currentOnBudget
+      }
+    );
+    } catch(err) {
+      debug(err.stack);
+    }
+    client.close();
+    }());
+  });
+  return defaultRouter;
+};
+
+module.exports = router;
