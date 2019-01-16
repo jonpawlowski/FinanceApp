d792a62210de5c73c20dc0081074bc2fae463f06
jonpawlowski <jon.pawlowski@gmail.com>
2019-01-16 04:11:11 +0000

    HTML cleanup and added new payment types

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -1,6 +1,6 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $.fn.dataTable.moment('MM/DD/YYYY');
+  $.fn.allChargesTable.moment('MM/DD/YYYY');
   $('#recentChargesTables').DataTable(
     {
         "order": [[ 0, "desc" ]]
