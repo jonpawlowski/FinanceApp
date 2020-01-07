608b238114759517f23179ef00b9706339929000
jonpawlowski <jon.pawlowski@gmail.com>
2020-01-07 03:17:14 +0000

    Missed one piece for the new recurring charges

--- src/routes/chargeRoutes.js
+++ src/routes/chargeRoutes.js
@@ -248,12 +248,30 @@ function router() {
 
         }
 
+        // Get current month to display on index page
+        const d = new Date();
+        const month = new Array();
+        month[0] = "January";
+        month[1] = "February";
+        month[2] = "March";
+        month[3] = "April";
+        month[4] = "May";
+        month[5] = "June";
+        month[6] = "July";
+        month[7] = "August";
+        month[8] = "September";
+        month[9] = "October";
+        month[10] = "November";
+        month[11] = "December";
+        const currentMonth = month[d.getMonth()];
+
         res.render(
           'recurringChargeFormView',
           {
             commentsList,
             todaysDate,
-            recurringCharges
+            recurringCharges,
+            currentMonth
           }
         );
       }
